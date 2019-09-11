7d3a61dbba4eb3e8754b2b0c75d7254f4b3ce0f0
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-11 17:30:49 +0100

    Update styles, create component for loading screen and create nav links

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, {useState, useEffect} from "react";
 import "./App.css";
 import axios from 'axios';
 import Nav from "./Components/Navigation/nav";
-import Header from "./Components/Header/Header"
+import Header from "./Components/Header/Header";
+import Body from "./Components/Body/body";
 
 const nasaApi = 'https://lambda-github-api-server.herokuapp.com/';
 
 function App() {
 
-  const [headerState, setHeaderState] = useState({})
+  const [headerState, setHeaderState] = useState()
   useEffect(() =>{
     axios.get(nasaApi)
     .then((response) =>{
@@ -20,12 +21,18 @@ function App() {
     })
   }, []);
 
-  return (
-    <div className="App">
-      <Nav />
-      <Header props= {headerState}/>
+  return headerState //if the headerState is undefined, then...
+  ?
+    (
+      <div className="App">
+        <Nav/>
+        <Header props= {headerState}/>
+        <Body props= {headerState}/>
+      </div>
+    )
+  : <div>  {/*else headerState run after getting the data from the api*/}
+      <h3>Loading...</h3>
     </div>
-  );
 }
 
 export default App;
--- src/Components/Navigation/nav.js
+++ src/Components/Navigation/nav.js
@@ -3,8 +3,14 @@ import nasa from './img/nasa.jpg';
 
 function Nav () {
   return (
-    <div>
-      <img src={nasa} alt="nasa logo"/>
+    <div className="navigation">
+      <img className="App-logo" src={nasa} alt="nasa logo"/>
+      <div className="navs">
+          <nav className="App-link">Home</nav>
+          <nav className="App-link">About</nav>
+          <nav className="App-link">Works</nav>
+          <nav className="App-link">Contact</nav>
+      </div>
     </div>
   );
 }
