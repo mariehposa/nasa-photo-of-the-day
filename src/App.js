0d2d70893ce99b0481a16046d6b4fa5ddb6c61ea
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-11 14:21:12 +0100

    import header to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
+import Nav from "./Components/Navigation/nav";
+import Header from "./Components/Header/Header"
+
+const nasaApi = 'https://lambda-github-api-server.herokuapp.com/';
 
 function App() {
+  useEffect(() =>{
+    axios.get(nasaApi)
+    .then((response) =>{
+      console.log(response.data);
+    })
+    .catch((error) =>{
+      console.log(error.message);
+    })
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <Nav />
+      <Header/>
     </div>
   );
 }
---
+++ src/Components/Navigation/nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import nasa from './img/nasa.jpg';
+
+function Nav () {
+  return (
+    <div>
+      <img src={nasa} alt="nasa logo"/>
+    </div>
+  );
+}
+
+export default Nav;
