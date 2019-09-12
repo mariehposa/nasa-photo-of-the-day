3983f8547321d0ca7b91bb119a5d2456f4f8f559
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-12 13:48:07 +0100

    import styled-component to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Header (props) {
     const {title, url, copyright} = props.props
     return (
       <div>
-        <img className="image" src={url} alt="Header image"/>
-        <div className="App-title">
+        <img src={url} alt="Header image"/>
+        <div>
             <h3>Title => {title} </h3>
             <p style={{"font-style":"italic"}}>Copyright: {copyright} </p>
         </div>
