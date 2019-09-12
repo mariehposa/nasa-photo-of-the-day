800d8a136e9caa312e06d8c2a8fa2afb0b503e8b
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-12 15:51:56 +0100

    add styles to footer

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -14,13 +14,12 @@ const Footerdiv = styled.div`
 
         a{
             width: 2rem;
-            color: white;
+            color: black;
             transition: all 0.3s;
 
             &:hover{
                 transition: all 0.3s;
-                color: black;
-                background:gold;
+                color: blue;
             }
         }
     }
@@ -29,7 +28,7 @@ const Footerdiv = styled.div`
 function Footer () {
     return(
         <Footerdiv>
-            <div class="footer-nav">
+            <div>
                 <a href="https://facebook.com" title="facebook">
                     <svg class="logo" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M15.117 0H.883C.395 0 0 .395 0 .883v14.234c0 .488.395.883.883.883h7.663V9.804H6.46V7.39h2.086V5.607c0-2.066 1.262-3.19 3.106-3.19.883 0 1.642.064 1.863.094v2.16h-1.28c-1 0-1.195.476-1.195 1.176v1.54h2.39l-.31 2.416h-2.08V16h4.077c.488 0 .883-.395.883-.883V.883C16 .395 15.605 0 15.117 0" fill-rule="nonzero"/></svg>
                 </a>
